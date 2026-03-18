81d1a594e20edb43e3408a635850b5144fb252f6
jaebeombag638-cyber <jaebeombag638@gmail.com>
2026-03-18 15:34:04 +0900

    Refactor CreateBoard to use CafeContext for user

--- src/components/CreateBoard.jsx
+++ src/components/CreateBoard.jsx
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-// import { useCafe } from './context/CafeContext';
+import { useCafe } from './context/CafeContext';
 const CreateBoard = () => {
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
     const navigate = useNavigate();
 
-    const storedUser = localStorage.getItem('currentUser');
-    const currentUser = storedUser ? JSON.parse(storedUser) : null;
-
-    // const { currentUser } = useCafe();
+    
+    const { currentUser } = useCafe();
     useEffect(() => {
         if (!currentUser) {
             alert("로그인 후 다시 이용해 주세요!");
