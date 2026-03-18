2f2f4f55d36c34dfc1f3ebedc0ddd084d5d4a20b
kjfcvx12 <kjfcvx12@gmail.com>
2026-03-18 18:59:12 +0900

    login edit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
     const {setCurrentUser}=useCafe();
 
     const [userId,setUserId]=useState('');
-    const [password,setPassword]=useState('');
+    const [pw,setPw]=useState('');
     const navigator=useNavigate();
 
     const onSubmit2=(e)=>{
@@ -14,7 +14,7 @@ const Login = () => {
         
         let users=JSON.parse(localStorage.getItem('users')) || [];
 
-        const loginUser=users.find((i)=>i.userId===userId&&i.pw===password);
+        const loginUser=users.find((i)=>i.userId===userId&&i.pw===pw);
 
         if(loginUser){
             setCurrentUser(loginUser);
@@ -22,7 +22,7 @@ const Login = () => {
             localStorage.setItem('currentUser',JSON.stringify(loginUser))
 
             setUserId('');
-            setPassword('');
+            setPw('');
 
             navigator('/list');
         }else{
@@ -35,7 +35,7 @@ const Login = () => {
             <form onSubmit={onSubmit2}>
                 <h1>로그인</h1>
                 <p><input type='text' onChange={(e)=>setUserId(e.target.value)} value={userId} name='userId' placeholder='아이디'/></p>
-                <p><input type='password' onChange={(e)=>setPassword(e.target.value)} value={password} name='password' placeholder='비밀번호'/></p>
+                <p><input type='password' onChange={(e)=>setPw(e.target.value)} value={pw} name='password' placeholder='비밀번호'/></p>
                 
                 <button type='submit'>로그인</button>
             </form>
